5cd5ef3cdc32c69eade5076bbea2c1336b0a1929
Bruno Pedroso <pedroso.bruno@gmail.com>
2016-08-17 21:24:25 -0300

    Create simple servie with swagger2

---
+++ src/main/java/com/bpedroso/poc/deploy/CdApiApplication.java
@@ -0,0 +1,12 @@
+package com.bpedroso.poc.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bpedroso/poc/deploy/CdApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bpedroso.poc.deploy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CdApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
